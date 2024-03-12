ab566c0c93812863edf94916f12606220939b24d
mehedidevs <mehedi.hasan.app.dev@gmail.com>
2024-03-12 21:56:53 +0600

    initial commit

---
+++ app/src/main/java/com/mehedi/mars_real_estate/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.mehedi.mars_real_estate
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * Our MainActivity is only responsible for setting the content view that contains the
+     * Navigation Host.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
